43e8a9d45f4c00ee396b152c4a36d01a6324a323
Urufupakku <70983646+Urufupakku@users.noreply.github.com>
2020-09-09 02:42:26 +0300

    Update hello.py

--- test/hello.py
+++ test/hello.py
@@ -1,10 +1,1 @@
 print("HelloWorld")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print 
-asdsad
-sadfsadf
-=======
->>>>>>> 39299df74cc0e0ab02ae6a4795c4463633e78653
-=======
->>>>>>> 39299df74cc0e0ab02ae6a4795c4463633e78653
