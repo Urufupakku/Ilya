a46518afe0407181727c38a5ad42de42db449e24
Urufupakku <70983646+Urufupakku@users.noreply.github.com>
2020-09-09 02:17:07 +0300

    Update test.sh

--- proga/test.sh
+++ proga/test.sh
@@ -4,3 +4,5 @@ a=$(curl --request GET \
    --url 'https://www.virustotal.com/vtapi/v2/url/report?apikey=c87205136d3d1e987c85d0cca44810c373c2589dd029db1328b8b45f4a74773b&resource='$i)
    echo $a
 done | jq>oufile.txt
+bla bla 
+bla
