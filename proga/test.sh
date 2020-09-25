45122b830b9628feadd75995a98c8614eb9f26fc
Ilya <UrufupakkuTeam@yandex.ru>
2020-09-25 21:58:32 +0300

    a

--- proga/test.sh
+++ proga/test.sh
@@ -4,3 +4,9 @@ a=$(curl --request GET \
    --url 'https://www.virustotal.com/vtapi/v2/url/report?apikey=c87205136d3d1e987c85d0cca44810c373c2589dd029db1328b8b45f4a74773b&resource='$i)
    echo $a
 done | jq>oufile.txt
+
+for i in $(cat ip.txt)
+do
+a=$(curl https://ipinfo.io/$i/json?token=60de712195395a)
+echo $a
+done | jq>file.txt
